3860f668e5f9234118e42e2044a3768775db4c58
Adeniran Akorede <akorede.adeniran2022@gmail.com>
2026-07-20 19:44:05 +0100

    Fix image filename casing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import profileImage from "../assets/adeniran-photo.JPG"
+import profileImage from "../assets/adeniran-photo.jpg"
 
 function Hero() {
   return (
